d95a33d5613dba0c5084d2b4c68e7b66143fc0ac
dygi79 <bartekdygas@poczta.onet.pl>
2023-03-22 08:00:43 +0100

    dodano klase TruckCar

--- CarsStore/Program.cs
+++ CarsStore/Program.cs
@@ -36,34 +36,63 @@ while (true)
     
 }
 
-var statisticsPassengerCar = passengerCar.GetStatistics();
+var truckCar = new TruckCar("Audi", "ciężarowy", 8000);
+truckCar.CarAdded += TruckCar_CarAdded;
+void TruckCar_CarAdded(object sender, EventArgs args)
+{
+    Console.WriteLine("Dodano nową ilość sprzedanych samochodów ciężarowych.");
+}
 
-/*float[] totalcars = { statisticsA1.Sum, statisticsA3.Sum, statisticsA4.Sum};
+while (true)
+{
+    Console.WriteLine("Podaj liczbe sprzedaych samochodów ciężarowych w kolejnych dniach. W celu zakończenia naciśnij litere w.");
+    Console.WriteLine("Uwaga, ze względów technicznych nasz magazyn może sprzedać jedynie 15 sztuk samochodów ciężarowych w ciągu jednego dnia.");
+    var input = Console.ReadLine();
+
+
+    if (input == "w")
+    {
+        break;
+    }
+    try
+    {
+        truckCar.AddCar(input);
+    }
+
+    catch (Exception ex)
+    {
+        Console.WriteLine($"{ex.Message}");
+    }
+
+}
+var statisticsPassengerCar = passengerCar.GetStatistics();
+var statisticsTruckCar = truckCar.GetStatistics();
+
+float[] totalcars = { statisticsPassengerCar.Sum, statisticsTruckCar.Sum};
 float statisticsTotalCar = 0;
 for (int i = 0; i < totalcars.Length; i++)
 {
     statisticsTotalCar += totalcars[i];
 }
 
-float[] totalprice = { statisticsA1.PriceForModel, statisticsA3.PriceForModel, statisticsA4.PriceForModel};
+float[] totalprice = { statisticsPassengerCar.PriceForModel, statisticsTruckCar.PriceForModel};
 float statisticsTotalPrice = 0;
 for (int i = 0; i < totalprice.Length; i++)
 {
     statisticsTotalPrice += totalprice[i];
 }
 
-var statisticsAveragePrice = statisticsTotalPrice / statisticsTotalCar; */
+var statisticsAveragePrice = statisticsTotalPrice / statisticsTotalCar;
 
 Console.WriteLine("========================================================================================================================");
 Console.WriteLine("=======================================================================================================================");
-Console.WriteLine($"Łącznie sprzedano  samochodów osobowych:{statisticsPassengerCar.Sum} sztuk, za łączną cenę : {statisticsPassengerCar.PriceForModel} euro. ");
+Console.WriteLine($"Łącznie sprzedano  samochodów osobowych:{statisticsPassengerCar.Sum} sztuk, za łączną cenę: {statisticsPassengerCar.PriceForModel} euro.");
 Console.WriteLine("=======================================================================================================================");
-
-
-/*Console.WriteLine("=======================================================================================================================");
-Console.WriteLine($"Łącznie sprzedano samochodów wsztykich modeli: {statisticsTotalCar} sztuk, za łączną cenę {statisticsTotalPrice} euro." );
+Console.WriteLine($"Łącznie sprzedano samochodów ciężarowych: {statisticsTruckCar.Sum} sztuk. za łączną cenę: {statisticsTruckCar.PriceForModel} euro.");
+Console.WriteLine("=======================================================================================================================");
+Console.WriteLine($"Łącznie sprzedano samochodów: {statisticsTotalCar} sztuk, za łączną cenę {statisticsTotalPrice} euro." );
 Console.WriteLine("========================================================================================================================");
-Console.WriteLine($"Średnia cena za jeden egzemplarz to : {statisticsAveragePrice:N2} euro.");*/
+Console.WriteLine($"Średnia cena za jeden egzemplarz to : {statisticsAveragePrice:N2} euro.");
 
 
 
---
+++ CarsStore/TruckCar.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CarsStore.CarBase;
+
+namespace CarsStore
+{
+    public class TruckCar : CarBase
+
+        {
+            private const string fileName = "truckcars.txt";
+            public override event CarAddedDelegate CarAdded;
+
+            //private List<float> cars = new List<float>();
+            public TruckCar(string carBand, string carModel, float carPrice)
+                : base(carBand, carModel, carPrice)
+            {
+
+            }
+            public override void AddCar(float car)
+            {
+                if (car >= 0 && car <= 15)
+                {
+                    using (var writer = File.AppendText(fileName))
+                    {
+                        writer.WriteLine(car);
+                    }
+                    //this.cars.Add(car);
+                    if (CarAdded != null)
+                    {
+                        CarAdded(this, new EventArgs());
+                    }
+                }
+                else
+                {
+                    throw new Exception("Podałeś nieprawidłową wartość.");
+                }
+            }
+            public override void AddCar(string car)
+            {
+                if (float.TryParse(car, out float result))
+                {
+                    this.AddCar(result);
+                }
+                else if (char.TryParse(car, out char letter))
+                {
+                    this.AddCar(letter);
+                }
+                else
+                {
+                    throw new Exception("Podaj prawidłową wartość.");
+
+                }
+            }
+            public override void AddCar(double car)
+            {
+                float carAsFloat = (float)car;
+                this.AddCar(carAsFloat);
+            }
+            public override void AddCar(int car)
+            {
+                float carAsInt = (int)car;
+                this.AddCar(carAsInt);
+            }
+
+            public override Statistics GetStatistics()
+            {
+                var carFromFile = this.ReadCarsFromFile();
+                var result = this.CountStatistics(carFromFile);
+                return result;
+            }
+
+            private List<float> ReadCarsFromFile()
+            {
+                var cars = new List<float>();
+                if (File.Exists($"{fileName}"))
+                {
+                    using (var reader = File.OpenText($"{fileName}"))
+                    {
+                        var line = reader.ReadLine();
+                        while (line != null)
+                        {
+                            var numer = float.Parse(line);
+                            cars.Add(numer);
+                            line = reader.ReadLine();
+                        }
+                    }
+                }
+                return cars;
+            }
+
+            private Statistics CountStatistics(List<float> cars)
+            {
+                var statistics = new Statistics();
+                statistics.Sum = 0;
+
+                foreach (var car in cars)
+                    if (car >= 0)
+                    {
+                        statistics.Sum += car;
+                        statistics.PriceForModel = statistics.Sum * CarPrice;
+
+                    }
+                return statistics;
+
+            }
+        }
+    }
+
+
+
+
+
